22e3df3b3151d5f3956631420c70ac8b60a43aa7
Arthur Lockman <hello@rthr.me>
2015-01-16 15:36:12 -0500

    Added program for part B of assignment.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: Lab1 num
+all: Lab1 num num_grades
 
 Lab1: Lab1.o
 	gcc Lab1.o -o Lab1
@@ -12,6 +12,12 @@ num: num.o
 num.o:
 	gcc -c num.c
 
+num_grades: num_grades.o
+	gcc num_grades.o -o num_grades
+
+num_grades.o:
+	gcc -c num_grades.c
+
 clean:
-	rm -f Lab1.o Lab1 num.o num
+	rm -f Lab1.o Lab1 num.o num num_grades.o num_grades
 
---
+++ num_grades.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This program reads in a number of student grades from the command line. 
+ * Usage:
+ *      ./num_grades [grade 1] [grade 2] ... [grade n]
+ * Example:
+ *      ./num_grades 95 97 83 84
+ */
+
+int main(int argc, const char* argv[])
+{
+    if (argc < 2) // Check that there was an entry
+    {
+        printf("Must enter a number on the command line!\n");
+        return 1; // Indicate failure
+    }
+    
+    int grades[argc - 1]; //initialize grades array with enough elements to hold all inputs
+    int numberOfGrades = 0; //initialize var to hold the number of grades entered
+    
+    /* Loop iterates once for each grade entered on the command line. 
+     * It terminates once each grade entered has been stored into the grades array.
+     */
+    for (int i = 1; i <= argc - 1; i++)
+    {
+        grades[i-1] = atoi(argv[i]); //store grade in array
+        printf("Grade: %d\n", grades[i-1]);
+        numberOfGrades++;
+    }
+
+    return 0; // Indicate success!
+}
+
