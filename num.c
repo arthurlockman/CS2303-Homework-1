1860c791e7cbae81433ecf91d53e693c449882e9
Arthur Lockman <hello@rthr.me>
2015-01-16 14:07:38 -0500

    Added comments to num.

--- num.c
+++ num.c
@@ -12,14 +12,14 @@ int main(int argc, const char* argv[])
 {
     int n; //Integer read from the command line goes in here.
 
-    if (argc < 2)
+    if (argc < 2) // Check that there was an entry
     {
         printf("Must enter a number on the command line!\n");
-        return 1;
+        return 1; // Indicate failure
     }
 
-    n = atoi(argv[1]);
+    n = atoi(argv[1]); // Get string from command line; convert to int
     printf("The number you entered was %d\n", n);
-    return 0;
+    return 0; // Indicate success!
 }
 
